243643798cdf62ebd0afc1ac2f51489ccf206785
sabrinaissa <93581344+sabrinaissa@users.noreply.github.com>
2021-11-16 18:37:25 +0300

    Add files via upload

---
+++ Stock.java
@@ -0,0 +1,28 @@
+public class Stock {
+     public static void main(String[] args) {
+         double share = 1000;
+         double purchase = 32.87;
+          double sold = 33.92;
+          double moneyPaid = purchase * share;
+          System.out.println("amount paid by joe is: " + moneyPaid);
+          double stockbrocker = 0.02 * purchase;
+          System.out.println("Amount of commision joe paid to broker: " + stockbrocker);
+          double moneySold = sold * share;
+          System.out.println("Amount sold by joe is: " + moneySold);
+          double stockbrockerSold = 0.02 * sold;
+          System.out.println("Amount commision joe paid to stocker is:  "+stockbrockerSold);
+          double MoneyLeft = moneySold - moneyPaid;
+          System.out.println("Money left is : " + MoneyLeft + " He get the profit");
+
+     }
+}
+
+OUTPUT
+PS C:\Users\SABRINA\Desktop\javaquestions> javac Stock.java
+PS C:\Users\SABRINA\Desktop\javaquestions> java Stock.java 
+amount paid by joe is: 32870.0
+Amount of commision joe paid to broker: 0.6574  
+Amount sold by joe is: 33920.0
+Amount commision joe paid to stocker is:  0.6784
+Money left is : 1050.0 He get the profit        
+PS C:\Users\SABRINA\Desktop\javaquestions> 
