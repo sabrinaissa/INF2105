42205aff7de59690090edace8a63d756a2158bd7
sabrina <issasabrina6@gmail.com>
2021-11-16 18:22:15 +0000

    Create Dist.java

---
+++ Dist.java
@@ -0,0 +1,20 @@
+public class Dist {
+    public static void main(String[] args) {
+        double distance,distancee, distanceee,speed,hours;
+        distance = 60 * 5;
+        System.out.println("The distance in 5 hours is: " + distance);
+        distancee = 60 * 8;
+        System.out.println("The distance in 8 hours is: " + distancee);
+        distanceee = 60 * 12;
+        System.out.println("The distance in 12 hours is: " + distanceee);
+    }
+}
+
+OUTPUT
+
+PS C:\Users\SABRINA\Desktop\javaquestions> javac Dist.java
+PS C:\Users\SABRINA\Desktop\javaquestions> java Dist      
+The distance in 5 hours is: 300.0
+The distance in 8 hours is: 480.0 
+The distance in 12 hours is: 720.0
+PS C:\Users\SABRINA\Desktop\javaquestions> 
