3e64eae261fd7473ed0f1f01ee57b3ee5f267695
sabrinaissa <93581344+sabrinaissa@users.noreply.github.com>
2021-11-16 18:35:58 +0300

    Add files via upload

---
+++ Personnel.java
@@ -0,0 +1,18 @@
+public class Personnel{
+    public static void main(String[] args) {
+        long phone = 772703470L;
+        System.out.println("Sabrina");
+        System.out.println("Mtopepo" +" "+ "Zanzibar"+" " + "Mtopepo"+" " + 124D );
+        System.out.println(phone);
+        System.out.println("STATE UNIVERSITY OF ZANZIBAR .SUZA");
+        
+    }
+}
+
+OUTPUT
+PS C:\Users\SABRINA\Desktop\javaquestions> javac  Personnel.java
+PS C:\Users\SABRINA\Desktop\javaquestions> java  Personnel      
+Sabrina
+Mtopepo Zanzibar Mtopepo 124.0
+772703470
+STATE UNIVERSITY OF ZANZIBAR .SUZA
