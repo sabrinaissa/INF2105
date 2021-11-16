2d3fa4de3c8ddf4b9651455dec58e88b4daf6c7e
sabrinaissa <93581344+sabrinaissa@users.noreply.github.com>
2021-11-16 18:35:22 +0300

    Add files via upload

---
+++ Order.java
@@ -0,0 +1,18 @@
+public class Order {
+    public static void main(String[] args) {
+        double xi = 7 + (3 * (6/2)) -1;
+        System.out.println(xi);
+        double xii = 2%2 + (2*2) -(2/2);
+        System.out.println(xii);
+        double xiii = (3*9 *(3+(9*3/(3))));
+        System.out.println(xiii);
+    }
+}
+
+OUTPUT
+PS C:\Users\SABRINA\Desktop\javaquestions> javac Order.java     
+PS C:\Users\SABRINA\Desktop\javaquestions> java Order      
+15.0
+3.0  
+324.0
+PS C:\Users\SABRINA\Desktop\javaquestions>  
